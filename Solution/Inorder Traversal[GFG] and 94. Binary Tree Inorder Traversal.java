fec96984c396770dcd1c18fdb199904e17e6db15
Rohit <90521597+Rohit311098@users.noreply.github.com>
2022-03-05 13:27:00 +0530

    Update Inorder Traversal[GFG] and 94. Binary Tree Inorder Traversal.java

--- Solution/Inorder Traversal[GFG] and 94. Binary Tree Inorder Traversal.java
+++ Solution/Inorder Traversal[GFG] and 94. Binary Tree Inorder Traversal.java
@@ -31,3 +31,90 @@ class Solution {
 
 
 2. Iterative Approach
+
+class Solution {
+    // Function to return a list containing the inorder traversal of the tree.
+    public class Pair
+    {
+        Node node;
+        char state;
+        
+        Pair(Node node)
+        {
+            this.node=node;
+            this.state='l';
+        }
+    }
+    ArrayList<Integer> inOrder(Node root) {
+        
+        if(root==null)
+        {
+            return new ArrayList<>();
+        }
+        
+        ArrayList<Integer> result=new ArrayList<>();
+         Stack<Pair> stk=new Stack<>();
+        stk.push(new Pair(root));
+        
+        while(stk.size()!=0)
+        {
+            if(stk.peek().state=='l')
+            {
+                if(stk.peek().node.left!=null)
+                {
+                    Pair p=stk.pop();
+                    p.state='i';
+                    stk.push(p);
+                    stk.push(new Pair(p.node.left));
+                }
+                else
+                {
+                    Pair p=stk.pop();
+                    p.state='i';
+                    stk.push(p);
+                }
+                
+            }
+            else if(stk.peek().state=='i')
+            {
+                result.add(stk.peek().node.data);
+                stk.peek().state='r';
+            }
+            else
+            {
+                Pair top=stk.pop();
+                if(top.node.right!=null)
+                {
+                    stk.push(new Pair(top.node.right));
+                }
+            }
+        }
+        
+        return result;
+    }
+}
+
+3. Morris Traversal
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
