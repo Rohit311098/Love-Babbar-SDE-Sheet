bee7aaa5de96311d75a86d404c8e2fb36a37e147
Rohit <90521597+Rohit311098@users.noreply.github.com>
2022-03-06 17:54:40 +0530

    Update Postorder Traversal[GFG] & 145. Binary Tree Postorder Traversal.java

--- Solution/Postorder Traversal[GFG] & 145. Binary Tree Postorder Traversal.java
+++ Solution/Postorder Traversal[GFG] & 145. Binary Tree Postorder Traversal.java
@@ -35,3 +35,64 @@ class Solution {
 }
 
 2. Iterative Method
+class Solution {
+    
+    public class Pair
+    {
+        TreeNode node;
+        char state;
+        
+        Pair(TreeNode node)
+        {
+            this.node=node;
+            this.state='l';
+        }
+    }
+    public List<Integer> postorderTraversal(TreeNode root) {
+        
+        if(root==null)
+        {
+            return new ArrayList<>();
+        }
+        
+        List<Integer> result=new ArrayList<>();
+        Stack<Pair> stk=new Stack<>();
+        stk.push(new Pair(root));
+        
+        while(stk.size()>0)
+        {
+            Pair top=stk.peek();
+            if(top.state=='l')
+            {
+                if(top.node.left!=null)
+                {
+                    top.state='r';
+                    stk.push(new Pair(top.node.left));
+                }
+                else
+                {
+                    top.state='r';
+                }
+            }
+            else if(stk.peek().state=='r')
+            {
+                if(top.node.right!=null)
+                {
+                    top.state='p';
+                    stk.push(new Pair(top.node.right));
+                }
+                else
+                {
+                    top.state='p';
+                }
+            }
+            else
+            {
+                result.add(top.node.val);
+                stk.pop();
+            }
+        }
+        return result;
+    }
+}
+
