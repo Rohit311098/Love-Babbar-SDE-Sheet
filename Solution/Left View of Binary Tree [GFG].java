c386cb42db7f28c1af29cc1737e69da7146ab0c8
Rohit <90521597+Rohit311098@users.noreply.github.com>
2022-03-07 15:46:49 +0530

    DFS and BFS Approach

---
+++ Solution/Left View of Binary Tree [GFG].java
@@ -0,0 +1,41 @@
+1. DFS Approach
+TC-O(N)   SC-O(1)
+
+class Tree
+{
+    public void DFS(Node root,ArrayList<Integer> result,int level)
+    {
+        if(root==null)
+        {
+            return;
+        }
+        
+        if(result.size()<level)
+        {
+            result.add(root.data);
+        }
+        
+        DFS(root.left,result,level+1);
+        DFS(root.right,result,level+1);
+        
+    }
+    ArrayList<Integer> leftView(Node root)
+    {
+        if(root==null)
+        {
+           return new ArrayList<>(); 
+        }
+      ArrayList<Integer> result=new ArrayList<>();
+      
+      DFS(root,result,1);
+      
+      return result;
+    }
+}
+
+2. BFS Approach
+
+
+
+
+
