3ad51b32621d9ae4315e8cd62f7526c00e59ed21
Rohit <90521597+Rohit311098@users.noreply.github.com>
2022-03-09 09:08:17 +0530

    Update Left View of Binary Tree [GFG].java

--- Solution/Left View of Binary Tree [GFG].java
+++ Solution/Left View of Binary Tree [GFG].java
@@ -1,4 +1,42 @@
-1. DFS Approach
+1. BFS Approach
+ TC-O(N)  SC-O(N)
+class Tree
+{
+    //Function to return list containing elements of left view of binary tree.
+    ArrayList<Integer> leftView(Node root)
+    {
+        if(root==null)
+        {
+            return new ArrayList<>();
+        }
+      ArrayList<Integer> result=new ArrayList<>();
+      Queue<Node> queue=new ArrayDeque<>();
+      queue.add(root);
+      while(queue.size()>0)
+      {
+          result.add(queue.peek().data);
+          int length=queue.size();
+          while(length-->0)
+          {
+              Node curr=queue.remove();
+              if(curr.left!=null)
+              {
+                  queue.add(curr.left);
+              }
+              if(curr.right!=null)
+              {
+                  queue.add(curr.right);
+              }
+          }
+      }
+      return result;
+    }
+}
+
+
+
+
+2. DFS Approach
 TC-O(N)   SC-O(1)
 
 class Tree
