b82dd176567e0c0243f23b7f64c1116a22136e1b
Rohit <90521597+Rohit311098@users.noreply.github.com>
2022-03-15 11:18:55 +0530

    TC-O(

---
+++ BackTracking/Rat in a Maze Problem - I.java
@@ -0,0 +1,43 @@
+class Solution {
+    
+    static ArrayList<String> result;
+        
+    public static  void helper(int[][] m,int sr,int sc,int dr,int dc,String currpath)
+    {
+        if(sr<0 || sr>dr || sc<0 || sc>dc || m[sr][sc]==0 || m[sr][sc]==-1)
+        {
+            return;
+        }
+        
+        if(sr==dr && sc==dc)
+        {
+            result.add(currpath);
+            return;
+        }
+        
+        m[sr][sc]=-1;
+        
+        //Upward call
+        
+        helper(m,sr-1,sc,dr,dc,currpath+'U');
+        
+        //Downward call
+        
+        helper(m,sr+1,sc,dr,dc,currpath+'D');
+        
+        //Left call
+        
+        helper(m,sr,sc-1,dr,dc,currpath+'L');
+        
+        //Right call
+        
+        helper(m,sr,sc+1,dr,dc,currpath+'R');
+        
+        m[sr][sc]=1;
+    }
+    public static ArrayList<String> findPath(int[][] m, int n) {
+        result=new ArrayList<>();
+        helper(m,0,0,n-1,m[0].length-1,"");
+        return result;
+    }
+}
