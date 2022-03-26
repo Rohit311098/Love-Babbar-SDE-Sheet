4bffe0ff5275332e92f3629e0ebfe703b755042e
Rohit <90521597+Rohit311098@users.noreply.github.com>
2022-03-27 00:49:31 +0530

    Create Kth largest element in BST .java

---
+++ BST/Kth largest element in BST .java
@@ -0,0 +1,34 @@
+class Solution
+{
+    // return the Kth largest element in the given BST rooted at 'root'
+    int globalk=0;
+    public int kthLargest(Node root,int k)
+    {
+        globalk=k;
+        
+        return reverseinorder(root);
+    }
+    
+    public int reverseinorder(Node root)
+    {
+        if(root==null)
+        {
+            return -1;
+        }
+        
+        int ans=reverseinorder(root.right);
+        if(ans!=-1)
+        {
+            return ans;
+        }
+        
+        globalk--;
+        
+        if(globalk==0)
+        {
+            return root.data;
+        }
+        
+        return reverseinorder(root.left);
+    }
+}
