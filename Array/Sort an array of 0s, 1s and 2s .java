44e1ee41992a826bbae6c43eb5ac4dfe2b5b5db6
Rohit <90521597+Rohit311098@users.noreply.github.com>
2023-05-07 23:07:46 +0530

    Update Sort an array of 0s, 1s and 2s .java

--- Array/Sort an array of 0s, 1s and 2s .java
+++ Array/Sort an array of 0s, 1s and 2s .java
@@ -1,3 +1,61 @@
+Approach 1: Count number of 0s,1s and 2's and at last just replace array with those numbers of 0s,1s,2s; 
+    
+TC-O(2N)  SC-0(1)
+
+
+class Solution
+{
+    public static void sort012(int a[], int n)
+    {
+        int zero=0,ones=0,two=0;
+        
+        for(int i:a)
+        {
+            if(i==0)
+            {
+                zero++;
+            }
+            else if(i==1)
+            {
+                ones++;
+            }
+            else if(i==2)
+            {
+                two++;
+            }
+        }
+        
+        int k=0;
+        while(zero>0)
+        {
+            a[k]=0;
+            k++;
+            zero--;
+        }
+        
+        
+        while(ones>0)
+        {
+            a[k]=1;
+            k++;
+            ones--;
+        }
+        
+         while(two>0)
+        {
+            a[k]=2;
+            k++;
+            two--;
+        }
+    }
+}
+    
+    
+   
+
+Approach 2:
+
+
 class Solution
 {
     public static void sort012(int a[], int n)
