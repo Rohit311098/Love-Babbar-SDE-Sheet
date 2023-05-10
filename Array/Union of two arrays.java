83ebd9c17a23f79787af7c8771a1dac2412384da
Rohit <90521597+Rohit311098@users.noreply.github.com>
2023-05-10 22:16:31 +0530

    Update Union of two arrays.java

--- Array/Union of two arrays.java
+++ Array/Union of two arrays.java
@@ -1,3 +1,54 @@
+1. Using ArrayList and checking elements in arraylist if they are present or not. If not present add them else leave them.
+    TC-O(n*n+m*m)    SC-O(n+m)                                              
+    
+class Solution{
+    public static int doUnion(int a[], int n, int b[], int m) 
+    {
+         int total=0;        
+        ArrayList<Integer> arr=new ArrayList<>();
+        for(int i=0;i<n;i++)
+        {
+            int flag=0;
+            for(int j=0;j<arr.size();j++)
+            {
+                if(arr.get(j)==a[i])
+                {
+                    flag=1;
+                    break;
+                }
+            }
+            
+            if(flag==0)
+            {
+                arr.add(a[i]);
+            }
+        }
+        
+        for(int i=0;i<m;i++)
+        {
+            int flag=0;
+            for(int j=0;j<arr.size();j++)
+            {
+                if(arr.get(j)==b[i])
+                {
+                    flag=1;
+                    break;
+                }
+            }
+            
+            if(flag==0)
+            {
+                arr.add(b[i]);
+            }
+        }
+        return arr.size();
+    }
+    
+}
+
+
+
+
 1. Using HashSet Extra Space
 class Solution{
     public static int doUnion(int a[], int n, int b[], int m) 
