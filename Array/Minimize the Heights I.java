87785427099deb22b0ee47e600ab96fb91a08e7d
Rohit <90521597+Rohit311098@users.noreply.github.com>
2023-05-14 14:07:42 +0530

    Update Minimize the Heights I.java

--- Array/Minimize the Heights I.java
+++ Array/Minimize the Heights I.java
@@ -26,6 +26,81 @@ class Solution {
 2. Using Greedy Method- 
   The idea behind this approach is to increase or decrease the heights of the towers in a way that moves the towers closer to the average height. 
   By doing this, we can minimize the difference between the maximum and minimum heights.
+    
+    public static int getMinDiff2(int[] arr, int n, int k) {
+	        
+	        int min_height=get_min(arr,n);
+	        int max_height=get_max(arr,n);
+	        
+	        int old_diff=max_height-min_height;
+	        
+	        System.out.println("Initial difference"+old_diff);
+	        
+	        int avg=(max_height+min_height)/2;
+	        System.out.println("Average height"+avg);
+	        
+	        for(int i=0;i<n;i++)
+	        {
+	            if(arr[i]>avg)
+	            {
+	                arr[i]-=k;
+	            }
+	            else
+	            {
+	                arr[i]+=k;
+	            }
+	        }
+	        
+	        System.out.println("Array after changing");
+	        
+	        for(int i=0;i<n;i++)
+	        {
+	           System.out.print(arr[i]+" ");
+	        }
+	        System.out.println();
+	        int new_min_height=get_min(arr,n);
+	        int new_max_height=get_max(arr,n);
+	        
+	        System.out.println("New Maximum of array"+new_max_height);
+	        System.out.println("New Minimum of array"+new_min_height);
+	        
+	        int new_diff=new_max_height-new_min_height;
+	        System.out.println("New difference"+new_diff);
+	        
+	        return (old_diff > new_diff)?new_diff:old_diff;
+	     
+	         
+	    }
+	    
+	    public static int get_min(int[] arr,int n)
+	    {
+	        int min_element=Integer.MAX_VALUE;
+	        
+	        for(int i=0;i<n;i++)
+	        {
+	            if(min_element>arr[i])
+	            {
+	                min_element=arr[i];
+	            }
+	        }
+	        System.out.println("Minimum element "+min_element);
+	        return min_element;
+	    }
+	    
+	    public static int get_max(int[] arr,int n)
+	    {
+	        int max_element=Integer.MIN_VALUE;
+	        
+	         for(int i=0;i<n;i++)
+	        {
+	            if(max_element<arr[i])
+	            {
+	                max_element=arr[i];
+	            }
+	        }
+	         System.out.println("Maximum element "+max_element);
+	        return max_element;
+	    }
   
   
   
