a00e4289251522478529c375c6b54ef197fb8b8c
Rohit <90521597+Rohit311098@users.noreply.github.com>
2023-05-17 19:48:03 +0530

    Update 287. Find the Duplicate Number.java

--- Array/287. Find the Duplicate Number.java
+++ Array/287. Find the Duplicate Number.java
@@ -1,4 +1,12 @@
-Approach 1: using Hashset just check which number repeates and return it.
+Brute Force Approach-> Using Two loops just traverse and check if that element is present or not.
+  TC-O(N2)   SC-O(1)
+
+Approach 2: using Hashset just check which number repeates and return it  or you can use another array and place element with the help of index.
+
+  TC-O(N)  SC-O(N)
+  
+  
+  Using Hashset:
   class Solution {
     public int findDuplicate(int[] nums) {
 
@@ -21,5 +29,25 @@ Approach 1: using Hashset just check which number repeates and return it.
     }
 }
 
+Using Extra Array:
 
-Approach 2: Using Index of element
+class Solution {
+    public int findDuplicate(int[] nums) {
+
+        int[] dup=new int[nums.length];
+
+        for(int i=0;i<nums.length;i++)
+        {
+          if(dup[nums[i]-1]==0)
+          {
+            dup[nums[i]-1]=nums[i];
+          }
+          else
+          {
+            return nums[i];
+          }
+        }
+
+        return -1;
+    }
+}
