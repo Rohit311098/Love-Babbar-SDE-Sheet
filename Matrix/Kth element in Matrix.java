c4b87a8162d7d760b08727068a02d28050e1388d
Rohit <90521597+Rohit311098@users.noreply.github.com>
2023-05-29 22:08:28 +0530

    Update Kth element in Matrix.java

--- Matrix/Kth element in Matrix.java
+++ Matrix/Kth element in Matrix.java
@@ -1,3 +1,34 @@
 1. Brute Force Approach: Use extra space and sort it then return arr[k-1] element.
   
-2. Optimised Approach:
+2. Optimised Approach: TC-O(N^2 LOGN)   SC-O(N)
+  
+  class Solution {
+    public int kthSmallest(int[][] matrix, int k) {
+      int n=matrix.length;
+      Map<Integer,Integer> map=new TreeMap<>();
+
+      for(int i=0;i<n;i++)
+      {
+        for(int j=0;j<n;j++)
+        {
+          map.put(matrix[i][j],map.getOrDefault(matrix[i][j],0)+1);
+        }
+      }
+
+      for(int x:map.keySet())
+      {
+           k-=map.get(x);
+
+           if(k<=0)
+           {
+             return x;
+           }
+      }
+
+      return -1;
+        
+    }
+}
+
+
+3. Optimised Approach 3: 
