0c813bb2439969b3709fbe48004c776e275179b1
Rohit <90521597+Rohit311098@users.noreply.github.com>
2023-05-29 23:11:50 +0530

    Update Common elements in all rows of a given matrix.java

--- Matrix/Common elements in all rows of a given matrix.java
+++ Matrix/Common elements in all rows of a given matrix.java
@@ -1,3 +1,31 @@
 1. Brute Force Approah: Traverse first row and search each element in all row. TC-O(N*(N*M))  SC-O(1)
   
-  2. Optimised Approach:
+  2. Optimised Approach:   TC-O(N*M)  SC-O(N)
+
+public static int[] commonelement(int[][] matrix,int n)
+{
+   HashMap<Integer,Integer> hm=new HashMap<>();
+  
+  ArrayList<Integer> result=new ArrayList<>();
+  for(int i=0;i<n;i++)
+  {
+    hm.put(matrix[0][i],1);
+  }
+  
+  for(int i=1;i<n;i++)
+  {
+     for(int j=0;j<n;j++)
+     {
+       if(hm.get(matrix[i][j])!=null && hm.get(matrix[i][j])==i)
+          {
+            hm.put(matrix[i][j],hm.get(matrix[i][j]+1));
+            if(i==n-1)
+            {
+               result.add(matrix[i][j]);
+            }
+                   
+          }
+     }
+  }
+  return result; 
+}
